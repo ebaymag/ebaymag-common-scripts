b61b5346a646aad70529874d3e0305554c29f807
yanmeng <974166167@qq.com>
2025-03-17 13:26:19 +0800

    帮卖家收到导入商品

---
+++ 导入常用语句.rb
@@ -0,0 +1,3 @@
+# 首先在页面上创建一个导入的task,然后执行下面的方法，帮卖家手动推送
+ProductImport::Prepare.call(Account.find_by_name(name).user.product_import) unless Account.find_by_name(name).user.product_import
+Account.find_by_name(name).user.product_import.items.each do |item| ProductImport::Process.call(item)  end
