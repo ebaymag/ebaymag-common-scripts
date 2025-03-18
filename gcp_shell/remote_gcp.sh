b70968fcb04a5420018d64577d865f88553fdd36
yanmeng <974166167@qq.com>
2025-03-18 09:57:05 +0800

    相关shell脚本

--- gcp_shell/remote_gcp.sh
+++ gcp_shell/remote_gcp.sh
@@ -1,7 +1,9 @@
 #使用这个脚本可以连接相关环境的数据库，使用方式，将cd 后面的信息替换为ebaymag的项目地址，
 #然后使用
 # sh remote_gcp.sh gamma
-# 上面这个命令链接gamma环境
+# 上面这个命令链接gamma环境，然后通过
+# psql $DATABASE_URL
+#就可以连接
 echo $1
 if [ ! $1 ]; then
 env="prod"
