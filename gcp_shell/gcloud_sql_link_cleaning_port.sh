c809b15b6db436521367fa93750d09653f479e80
yanmeng <974166167@qq.com>
2025-03-19 15:41:06 +0800

    链接数据库

---
+++ gcp_shell/gcloud_sql_link_cleaning_port.sh
@@ -0,0 +1,16 @@
+#通过port方法链接GCP clone数据库
+#!/bin/bash
+
+# 查找使用端口5434的进程ID
+pid=$(lsof -ti tcp:5435)
+
+# 检查是否有进程使用该端口
+if [ -z "$pid" ]; then
+  echo "没有进程使用端口5434。"
+else
+  # 杀掉使用该端口的进程
+  kill -9 $pid
+  echo "已杀掉使用端口5434的进程：$pid。"
+fi
+
+/Users/yanmeng/Documents/google-cloud-sdk/bin/cloud_sql_proxy -instances=ebay-mag:europe-west4:data-cleaning=tcp:5435
