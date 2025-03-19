670109d7b86ab2d47544ce2e3606da359a951473
yanmeng <974166167@qq.com>
2025-03-19 18:22:27 +0800

    链接数据库

--- gcp_shell/wconsole_gcp.sh
+++ gcp_shell/wconsole_gcp.sh
@@ -5,6 +5,8 @@ else
 env=$1
 fi
 echo $env
-#下面的改为你的ebaymag的项目文件夹
+#下面的改为你的ebaymag的项目文件夹，
+#假如进入gamma环境，运行 sh wconsole_gcp.sh gamma
+#其他的环境为：beta,alpha,prod
 cd /Users/yanmeng/Documents/workplace/ruby/ebay-mag-2-4
 bin/console $env
